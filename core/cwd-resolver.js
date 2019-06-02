9dfbeb10cfac49de772e9bd93f3e870fc9a7de0f
Ali Fırat Güler <fenerim.net@gmail.com>
2019-06-02 22:42:44 +0300

    windows ve linux dosya sistemine uyumlua hale getirilidi

--- core/cwd-resolver.js
+++ core/cwd-resolver.js
@@ -2,7 +2,7 @@
 const path = require('path');
 
 function cwdResolver(dirname) {
-    if(dirname.indexOf('/snapshot') === -1) return dirname;
+    if(dirname.indexOf(path.sep + 'snapshot') === -1) return dirname;
     let execArr = process.execPath.split(path.sep);
     let cwdArr = execArr.slice(0, execArr.length - 1).map(x => x === '' ? path.sep : x);
     return path.join(...cwdArr);
