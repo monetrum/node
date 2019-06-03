6d0d0da001ab9cf1b72df884170f88aea6d21266
Ali Fırat Güler <fenerim.net@gmail.com>
2019-06-03 16:18:21 +0300

    smart contract islemlerinin dogrulamasi saglandi

--- src/workers/sync.js
+++ src/workers/sync.js
@@ -196,6 +196,11 @@ class Sync {
                         break txfor;
                     }
 
+                    if(tx.contract_wallet && !ecdsa.verify(tx.public_key, tx.from, tx.contract_sign)){
+                        console.log('imza geçersiz bu işlem smart contract tarafından yapılmamış', tx.seq, tx.hash);
+                        break;
+                    }
+
                     if(!tx.contract_wallet && ecdsa.addressFromPublicKey(tx.public_key) !== tx.from){
                         console.log('imza geçersiz', tx.seq, tx.hash);
                         sync = false;
