283e653f765fd9c3164ae0b59a89e45cd19e0d25
monetrum <admin@monetrum.org>
2019-06-03 12:06:32 +0300

    Klasör Yapısı Değiştirildi

---
+++ src/app.js
@@ -0,0 +1,100 @@
+'use strict';
+global.registry = require('./core/registry');
+global.cwd = require('./core/cwd-resolver')(__dirname);
+
+const cluster = require('cluster');
+const env = require('dotenv').config({ path: cwd + '/.env' }).parsed || { };
+const express = require('express');
+const app = express();
+const http = require('http');
+const httpServer = http.createServer(app);
+const bodyParser = require('body-parser');
+const requireDir = require('require-dir');
+const httpProxy = require('http-proxy');
+const validator = require('./validators/env');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+async function init(workerId){
+    await validator.env.validate(env);
+    let knex = require('knex')({ client: 'sqlite3', connection: { filename: cwd + '/data.db' }, useNullAsDefault: true });
+    let proxy = httpProxy.createProxyServer({ });
+    
+    proxy.on('error', e => console.error(e.message));
+    
+    registry.set('proxy', proxy);
+    registry.set('knex', knex);
+    registry.set('app', app);
+    registry.set('env', env);
+    registry.set('helpers', requireDir(__dirname + '/helpers', { recurse: true }));
+    registry.set('consts', requireDir(__dirname + '/consts', { recurse: true }));
+    registry.set('IP_WHITE_LIST', env.IP_WHITE_LIST.split(',').map(x => x.trim()));
+    //-----------------------------------------------------------------------------//
+
+    let { loader, createClient, stc } = registry.get('helpers');
+    let client = await createClient(env.MASTER_NODE_URL);
+    registry.set('client', client);
+    //------------------------------------------------------------------------------//
+    (new loader(app, __dirname + '/middlewares/app-level')).middlewares();
+    (new loader(app, __dirname + '/middlewares/router-level')).routers();
+    (new loader(app, __dirname + '/routes')).routers();
+    //------------------------------------------------------------------------------//
+    let { migration } = registry.get('consts');
+    //-----------------------------------------------------------------------------//
+    httpServer.listen(parseInt(env.LISTEN_PORT), env.LISTEN_HOST);
+    
+    if(workerId === 1){
+        await migration(knex);
+        let Sync = require('./workers/sync');
+        let sync = await new Sync(client, knex);
+        //----------------------------------------------------------------------------//
+        let txWorking = false;
+        let txIntervalcb = async () => {
+            if(txWorking === true){
+                return;
+            }
+
+            txWorking = true;
+            await stc(() => sync.txSynchronize());
+            txWorking = false;
+        };
+
+        //---------------------------------------------------------------------------//
+        let nodesWorking = false;
+        let nodesIntervalcb = async () => {
+            if(nodesWorking === true){
+                return;
+            }
+
+            nodesWorking = true;
+            await stc(() => sync.nodeSynchronize());
+            nodesWorking = false;
+        };
+
+        //---------------------------------------------------------------------------//
+        console.log('node listesi güncelleniyor');
+        await nodesIntervalcb();
+
+        setInterval(txIntervalcb, 3000);
+        setInterval(nodesIntervalcb, 30 * 60 * 1000);
+    }
+}
+
+if(cluster.isMaster){
+    let threads = env.THREADS ? parseInt(env.THREADS) : 2;
+    for (let i = 0; i < threads; i++) {
+        cluster.fork();
+    }
+
+    cluster.on('online', worker => console.log('worker ' + worker.process.pid + ' çalıştı'));
+    cluster.on('exit',(worker, code, signal) => {
+        console.log('worker ' + worker.process.pid + ' şu kod ile durdu: ' + code);
+        console.log('yeni worker başlatılıyor');
+        cluster.fork();
+    });
+
+} else {
+    init(cluster.worker.id).then(() => console.log('node başladı', env.LISTEN_HOST, env.LISTEN_PORT)).catch(e => console.error(e));
+}
+
---
+++ src/workers/sync.js
@@ -0,0 +1,245 @@
+'use strict';
+const { queries } = registry.get('consts');
+const request = require('request-promise-native');
+const { stc, ecdsa, blockchain, sleep, systeminfo } = registry.get('helpers');
+const env = registry.get('env');
+
+async function formatter(callback, field = undefined){
+    try {
+        let res = await callback();
+        let jsonres = typeof res === 'object' ? res : JSON.parse(res);
+        if(jsonres.status === 'error') throw new Error(jsonres.message);
+        return field ? jsonres[field] : jsonres;
+    } catch (e) {
+        return e; 
+    }
+}
+
+
+class Sync {
+    constructor(client, knex){
+        return new Promise( async (resolve, reject) => {
+            console.log('sistem bilgileriniz ve internet hızınız hesaplanıyor');
+            this.knex = knex;
+            this.client = client;            
+            let result = await stc(async () => await this.client.mutation(queries.addNode, { info: { ... await systeminfo(), port: parseInt(env.LISTEN_PORT), ssl: env.SSL === '1' }}));
+            if(result instanceof Error){
+                reject(result);
+            }
+
+            resolve(this);
+        });
+    }
+
+    async getBalance(address, asset){
+        let first = await this.knex.table('tx').where('from', address).where('asset', asset).whereIn('type', [ -2, 1, 2, 3, 4 ]).sum({ balance: 'amount'}).first();
+        return first.balance || 0.00;
+    }
+
+    async *nodesIterator(){
+        let lastId = 0;
+        while(true){
+            let nodes = await this.knex('nodes').where('id', '>', lastId).limit(1000);
+            if(nodes.length === 0){
+                break;
+            }
+
+            lastId = nodes[ nodes.length -1 ].id;
+            for(let node of nodes){
+                yield node;
+            }
+        }
+    }
+
+    async checkNodeSeq(ip, port, ssl = false){
+        return await formatter(() => request.get(`${ssl == true ? 'https' : 'http:'}//${ip}:${port}/tx/last-seq`), 'seq');
+    }
+
+    async reportConfirmRate(ip, port, ssl = false, seq){
+        return await formatter(() => request.post(`${ssl == true ? 'https' : 'http:'}//${ip}:${port}/tx/update-confirm-rate`, { json: { seq }}));
+    }
+
+    async lastSeq(){
+        let lastTx = await this.knex.table('tx').select(['seq']).orderBy('seq', 'DESC').limit(1).first();
+        return lastTx ? lastTx.seq : 0;
+    }
+
+    async *nodeTxIterator(ip, port, from = 0, ssl = false){
+        let lastSeq = from;
+        while(true){
+            let txes = await formatter(() => request.get(`${ssl == true ? 'https:' : 'http:'}//${ip}:${port}/tx/txes?from=${lastSeq}&limit=1000`), 'txes');
+            if(txes instanceof Error){
+                yield new Error('txes fetch failed');
+                return;
+            }
+
+            if(txes.length === 0){
+                break;
+            }
+
+            if(txes.find(tx => tx.type === -1 || tx.type === -2 )){
+                yield new Error('txes not completed');
+                return;
+            }
+
+            lastSeq = txes[ txes.length -1 ].seq;
+            for(let tx of txes){
+                yield tx;
+            }
+        }
+    }
+
+    async *mnTxIterator(seq){
+        let cursor = null;
+        while(true){
+            let resp = await stc(async () => (await this.client.query(queries.getTxList, { filters: { seq: { gt: seq } }, sorting: { _id: 'ASC', seq: 'ASC' }, limit: 100, cursor })));
+            if(resp instanceof Error){
+                yield new Error('txes fetch failed', node.ip, node.port);
+                return;
+            }
+
+            if(resp.tx.getTxList.transactions.length === 0){
+                break;
+            }
+
+            if(resp.tx.getTxList.transactions.find(tx => tx.type === -1 || tx.type === -2 )){
+                await sleep(2000);
+                continue;
+            }
+
+            cursor = resp.tx.getTxList.next_cursor;
+            for(let tx of resp.tx.getTxList.transactions){
+                delete tx.__typename;
+                tx.data = JSON.stringify(tx.data);
+                yield tx;
+            }
+        }
+    }
+
+
+    async nodeSynchronize(){
+        let cursor = null;
+        while(true){
+            let resp = await stc(async () => await this.client.query(queries.getNodes, { filters: { }, sorting: { _id: 'ASC' }, limit: 1000, cursor }));
+            if(resp instanceof Error){
+                console.log('nodes fetch failed', resp.message);
+                return;
+            }
+
+            cursor = resp.nodes.getNodes.next_cursor;
+            if(resp.nodes.getNodes.nodes.length === 0){
+                break;
+            }
+
+            for(let node of resp.nodes.getNodes.nodes){
+                if(node.ip !== env.LISTEN_HOST && node.port !== env.LISTEN_PORT){
+                    delete node.__typename;
+                    let localNode = await this.knex.table('nodes').where('ip', node.ip).where('port', node.port).first();
+                    if(localNode && node.accessible_service === false){
+                        await this.knex.table('nodes').where('id', localNode.id).delete();
+                        continue;
+                    }
+
+                    if(!localNode && node.accessible_service === true){
+                        await this.knex.table('nodes').insert({ ip: node.ip, port: node.port, ssl: node.ssl });
+                        continue;
+                    }
+                }
+            }
+        }
+    }
+
+    async txSynchronize(){
+        let sync = false;
+        let localSeq = await this.lastSeq();
+
+        nodesfor:
+        for await (let node of this.nodesIterator()){
+            let nodeLastSeq = await this.checkNodeSeq(node.ip, node.port, node.ssl);
+            if(nodeLastSeq instanceof Error){
+                continue;
+            }
+
+            if(nodeLastSeq <= localSeq){
+                continue;
+            }
+
+            let seq = await this.lastSeq();
+
+            txfor:
+            for await (let tx of this.nodeTxIterator(node.ip, node.port, seq, node.ssl)){
+                if(tx instanceof Error){
+                    sync = false;
+                    break txfor;
+                }
+
+                if(tx.type === 1){
+                    let balance = await this.getBalance(tx.from, tx.asset);
+                    if(balance < Math.abs(tx.amount)){
+                        console.log('balance yetersiz');
+                        sync = false;
+                        break txfor;
+                    }
+
+                    if(!ecdsa.verify(tx.public_key, `${tx.from}__${tx.to}__${0 - Math.abs(tx.amount)}__${tx.asset}__${tx.nonce}`, tx.sign)){
+                        console.log('sign geçersiz', tx.hash, tx.sign);
+                        sync = false;
+                        break txfor;
+                    }
+
+                    if(tx.contract_wallet && ecdsa.addressFromPublicKey(tx.public_key) !== tx.contract_wallet){
+                        console.log('sign geçersiz', tx.hash, tx.sign);
+                        sync = false;
+                        break txfor;
+                    }
+
+                    if(!tx.contract_wallet && ecdsa.addressFromPublicKey(tx.public_key) !== tx.tx.from){
+                        console.log('sign geçersiz', tx.hash, tx.sign);
+                        sync = false;
+                        break txfor;
+                    }
+                }
+
+                let hash = blockchain.createHash({ prevHash: tx.prev_hash, from: tx.from, to: tx.to, amount: tx.amount, asset: tx.asset, nonce: tx.nonce });
+                if(hash !== tx.hash){
+                    console.log('hash geçersiz', hash + ' != ' + tx.hash);
+                    sync = false;
+                    break txfor;
+                }
+
+                let nodeAddress = `${node.ssl == true ? 'https:' : 'http:'}//${node.ip}:${node.port}`;
+                let inserted = await stc(async () => await this.knex.table('tx').insert({ ...tx, confirm_rate: tx.confirm_rate + 1, node: nodeAddress }));
+                if(inserted instanceof Error){
+                    throw new Error('işlem veritabanına eklenemedi', inserted.message);
+                }
+
+                await this.reportConfirmRate(node.ip, node.port, node.ssl, tx.seq);
+                console.log('yeni işlem ', tx.seq, tx.hash, node.ip, node.port);
+
+                if(tx.seq === nodeLastSeq){
+                    sync = true;
+                    break nodesfor;
+                }
+            }
+        }
+
+        if(sync === false){
+            let seq = await this.lastSeq();
+            for await (let tx of this.mnTxIterator(seq)){
+                if(tx instanceof Error){
+                    throw new Error('işlem çekme başarısız ' + tx.message);
+                }
+
+                let inserted = await stc(async () => await this.knex.table('tx').insert({ ...tx, confirm_rate: tx.confirm_rate + 1, node: 'masternode'}));
+                if(inserted instanceof Error){
+                    throw new Error('işlem veritabanına eklenemedi', inserted.message);
+                }
+
+                await stc(async () => await this.client.mutation(queries.updateConfirmRate, { seq: tx.seq }));
+                console.log('yeni işlem', tx.seq, tx.hash, 'masternode');
+            }
+        }
+    }
+}
+
+module.exports = Sync;
