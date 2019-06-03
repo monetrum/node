d56440ad88b743e1914a07caf49aa7041492cae9
monetrum <admin@monetrum.org>
2019-06-03 12:08:21 +0300

    fix

--- core/cwd-resolver.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-const path = require('path');
-const cwdarr = process.execPath.split('/');
-cwdarr.splice(cwdarr.length - 1, 1);
-
-module.exports = dirname => dirname.indexOf('/snapshot') !== -1 ? path.join('/', ...cwdarr) : dirname;
